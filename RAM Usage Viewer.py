38f99b776336de1e9a855b295e992b75d41880a8
Haramura <Haramura101@gmail.com>
2025-01-03 00:14:07 +0300

    Update RAM Usage Viewer.py

--- RAM Usage Viewer.py
+++ RAM Usage Viewer.py
@@ -1,40 +1,32 @@
 import tkinter as tk
 from tkinter import *
 import psutil
-import time
+
+def update_ram_info():
+    ram_info = psutil.virtual_memory()
+    total_label.config(text=f"Total: {ram_info.total / 1024 / 1024 / 1024:.2f} GB")
+    available_label.config(text=f"Available: {ram_info.available / 1024 / 1024 / 1024:.2f} GB")
+    used_label.config(text=f"Used: {ram_info.used / 1024 / 1024 / 1024:.2f} GB")
+    percentage_label.config(text=f"Percentage usage: {ram_info.percent}%")
+    window.after(100, update_ram_info)
 def main_window():
     global window
-    ram_info = psutil.virtual_memory()
-    # Create the main window
+    global total_label, available_label, used_label, percentage_label
+
     window = tk.Tk()
-    window.geometry("300x200")
+    window.geometry("250x120")
     window.title("RAM Usage Viewer")
     window.geometry(f"+{(window.winfo_screenwidth() - 200) // 2}+{(window.winfo_screenheight() - 200) // 2}")
     window.configure(background="white")
-
-
-    label = tk.Label(window, text=(f"Total: {ram_info.total / 1024 / 1024 / 1024:.2f} GB"), background="white", font="Arial 15", fg="black")
-    label.pack()
-    label = tk.Label(window, text=(f"Available: {ram_info.available / 1024 / 1024 / 1024:.2f} GB"), background="white", font="Arial 15", fg="black")
-    label.pack()
-    label = tk.Label(window, text=(f"Used: {ram_info.used / 1024 / 1024 / 1024:.2f} GB"), background="white", font="Arial 15", fg="black")
-    label.pack()
-    label = tk.Label(window, text=(f"Percentage usage: {ram_info.percent}%"), background="white", font="Arial 15", fg="black")
-    label.pack()
-
-    button = tk.Button(window, text="Refresh", command=refresh_window, background="red", font="Arial 15", fg="white")
-    button.pack()
-
-
-    # Run the application
+    total_label = tk.Label(window, text="", background="white", font="Arial 15", fg="black")
+    total_label.pack()
+    available_label = tk.Label(window, text="", background="white", font="Arial 15", fg="black")
+    available_label.pack()
+    used_label = tk.Label(window, text="", background="white", font="Arial 15", fg="black")
+    used_label.pack()
+    percentage_label = tk.Label(window, text="", background="white", font="Arial 15", fg="black")
+    percentage_label.pack()
+    update_ram_info()
     window.mainloop()
-def refresh_window():
-    time.sleep(0)
-    window.destroy()
+if __name__ == '__main__':
     main_window()
-
-if __name__ == '__main__': #первично вызываем главное окно при включении программы
-    main_window()
-    
-
-
